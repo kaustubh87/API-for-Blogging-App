40ae6d21c9d325ad981390004c2e42cee451aef2
kvin1087 <kaustubh87@gmail.com>
2018-04-23 22:40:36 -0700

    Added delete Route

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var _ = require('lodash');
 
-var jsonData = { count: 12, message: 'hey'};
+app.use(express.static('client'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
+//var jsonData = { count: 12, message: 'hey'};
+
+var lions = [];
+
+var id = 0;
+
+/*
 app.get('/', function(req,res){
 
     res.sendFile(__dirname+ '/index.html', function(err){
@@ -16,7 +27,35 @@ app.get('/', function(req,res){
 app.get('/data', function(req,res){
     res.json(jsonData);
 });
+*/
 
+app.get('/lions', function(req,res){
+    res.json(lions);
+});
+
+app.get('/lions/:id', function(req,res){
+    var id = req.params.id;
+    var lion = _.find(lions, {id: req.params.id});
+    res.json(lion || {});
+});
+
+app.post('/lions', function(req,res){
+    var lion = req.body;
+    id++;
+    lion.id = id + '';
+    lions.push(lion);
+    res.json(lion);
+});
+
+app.delete('lions/:id', function(req,res){
+    var id = req.params.id;
+    var lion = _.findIndex(lions, {id: req.params.id});
+    if(!lions[lion]){
+            res.send();
+    }else {
+        lions.splice(lion, 1);
+    }
+});
 
 app.listen(3000, function(req,res){
     console.log('Server is listening on port 3000');
